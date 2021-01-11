a8f7739d3c42a3ddc147530913c80ee9e0b40e52
mwlon <m.w.loncaric@gmail.com>
2021-01-10 19:53:08 -0500

    make it better

--- iterative_neural_networks/multiply.py
+++ iterative_neural_networks/multiply.py
@@ -12,37 +12,48 @@ from matplotlib import pyplot as plt
 @dataclass
 class Itermediate: # puns are a sign of good code, amirite
   inp: torch.Tensor
-  hidden: List[torch.Tensor]
+  hidden: torch.Tensor
+  bottleneck: torch.Tensor
   out: torch.Tensor
 
 class IternetMult(nn.Module):
-  def __init__(self, width):
+  def __init__(self, width, bottleneck_width, bias=True):
     super(IternetMult, self).__init__()
     # constants
     self.width = width
-    hidden_inp_size = 2 + 1 + width # 2 true inputs, 1 for 1's, width for hiddens
-    self.hidden_w_mask = torch.ones(hidden_inp_size, width)
+    self.bottleneck_width = bottleneck_width
+    self.bias = bias
+    hidden_inp_size = 2 + bottleneck_width # 2 true inputs, width for hiddens
+    if bias:
+      hidden_inp_size += 1
 
     # parameters
-    self.hidden_w = nn.Parameter(torch.normal(
+    self.wih = nn.Parameter(torch.normal(
       mean=torch.zeros(hidden_inp_size, width),
       std=1
     ) * np.sqrt(2.0 / width))
-    self.last = nn.Parameter(torch.normal(
+    self.whb = nn.Parameter(torch.normal(
+      mean=torch.zeros(width, bottleneck_width),
+      std=1
+    ) * np.sqrt(2.0 / bottleneck_width))
+    self.why = nn.Parameter(torch.normal(
       mean=torch.zeros(width, 1),
       std=1.0
     ) * np.sqrt(2.0))
 
 
   def init_itermediate(self, inp):
-    ones = torch.ones(inp.shape[0])[:, None]
-    expanded_inp = torch.cat([ones, inp], dim=1)
+    if self.bias:
+      ones = torch.ones(inp.shape[0])[:, None]
+      inp = torch.cat([ones, inp], dim=1)
     batch_size = inp.shape[0]
     hidden = torch.zeros(batch_size, self.width)
+    bottleneck = torch.zeros(batch_size, self.bottleneck_width)
     out = torch.zeros(batch_size, 1)
     return Itermediate(
-      expanded_inp,
+      inp,
       hidden,
+      bottleneck,
       out
     )
     
@@ -54,70 +65,12 @@ class IternetMult(nn.Module):
     return itermediate.out[:, 0]
 
   def iterate(self, itermediate):
-    inp = torch.cat([itermediate.inp, itermediate.hidden], dim=1)
-    adj_hidden_w = self.hidden_w * self.hidden_w_mask
-    normalized_hidden_w = adj_hidden_w / torch.sqrt(torch.sum(adj_hidden_w ** 2, dim=0)[None, :])
-    hidden = functional.relu(torch.matmul(inp, normalized_hidden_w))
-    out = torch.matmul(hidden, self.last)
-    return Itermediate(itermediate.inp, hidden, out)
-#class IternetMult(nn.Module):
-#  def __init__(self, widths):
-#    super(IternetMult, self).__init__()
-#    self.widths = widths
-#    self.layers = []
-#    prev_width = 3
-#    for i in range(len(widths)):
-#      width = widths[i]
-#      next_width = widths[i + 1] if i < len(widths) - 1 else 0
-#      layer = nn.Parameter(torch.normal(
-#        mean=torch.zeros(prev_width + next_width, width),
-#        std=1,
-#      ) * np.sqrt(2.0 / width))
-#      setattr(self, f'l{i}', layer)
-#      self.layers.append(layer)
-#      prev_width = width
-#    self.last = nn.Parameter(torch.normal(
-#      mean=torch.zeros(widths[-1], 1),
-#      std=1.0
-#    ) * np.sqrt(2.0))
-#
-#  def init_itermediate(self, inp):
-#    ones = torch.ones(inp.shape[0])[:, None]
-#    expanded_inp = torch.cat([ones, inp], dim=1)
-#    batch_size = inp.shape[0]
-#    hidden = [torch.zeros(batch_size, width) for width in self.widths]
-#    out = torch.zeros(batch_size, 1)
-#    return Itermediate(
-#      expanded_inp,
-#      hidden,
-#      out
-#    )
-#    
-#
-#  def forward(self, inp, iters):
-#    itermediate = self.init_itermediate(inp)
-#    for _ in range(iters):
-#      itermediate = self.iterate(itermediate)
-#    return itermediate.out[:, 0]
-#    
-#  def iterate(self, itermediate):
-#    hidden = []
-#    def compute_layer(inp, weight):
-#      normalized_weight = weight / torch.sqrt(torch.sum(weight ** 2, dim=0)[None, :])
-#      res = functional.relu(torch.matmul(inp, normalized_weight))
-#      hidden.append(res)
-#
-#    current_inp = torch.cat([itermediate.inp, itermediate.hidden[1]], dim=1)
-#    compute_layer(current_inp, self.l0)
-#
-#    for i in range(1, len(self.widths) - 1):
-#      current_inp = torch.cat([hidden[i - 1], itermediate.hidden[i + 1]], dim=1)
-#      compute_layer(current_inp, self.layers[i])
-#
-#    compute_layer(hidden[-1], self.layers[-1])
-#
-#    out = torch.matmul(hidden[-1], self.last)
-#    return Itermediate(itermediate.inp, hidden, out)
+    hidden_inp = torch.cat([itermediate.inp, itermediate.bottleneck], dim=1)
+    hidden = functional.relu(torch.matmul(hidden_inp, self.wih))
+    norm_bottle_weight = self.whb / torch.norm(self.whb, dim=0)[None, :]
+    bottleneck = torch.matmul(hidden, norm_bottle_weight)
+    out = torch.matmul(hidden, self.why)
+    return Itermediate(itermediate.inp, hidden, bottleneck, out)
 
 
 class DirectMult(nn.Module):
@@ -134,12 +87,13 @@ class DirectMult(nn.Module):
           std=1,
         ) * np.sqrt(2.0 / width))
       )
+      setattr(self, f'l{i}', self.layers[-1])
     self.last = nn.Parameter(torch.normal(
       mean=torch.zeros(width, 1),
       std=1.0
     ) * np.sqrt(2.0))
 
-  def forward(self, inp):
+  def forward(self, inp, **kwargs):
     ones = torch.ones(inp.shape[0])[:, None]
     x = torch.cat([ones, inp], dim=1)
     for i in range(len(self.widths)):
@@ -148,12 +102,22 @@ class DirectMult(nn.Module):
     out = torch.matmul(x, self.last)
     return out[:, 0]
 
-model = IternetMult(width=25)
-direct = DirectMult(widths=[175])
-batch_size = 111
+
+def new_main():
+  return IternetMult(width=70, bottleneck_width=3, bias=True)
+model = new_main()
+models = {
+  'direct': DirectMult(widths=[175]),
+  'direct_1': DirectMult(widths=[175]),
+  'direct_2': DirectMult(widths=[175]),
+  'main': model,
+  'main_1': new_main(),
+  'main_2': new_main(),
+}
+batch_size = 444
 train_iters = 5
 loss_fn = nn.MSELoss()
-lr = 0.1
+lr = 0.03
 train_batches = 1000
 max_iters = 20
 print('model params:')
@@ -162,37 +126,57 @@ for name, p in model.named_parameters():
 print('')
 losses = defaultdict(list)
 for i in range(train_batches):
-  model.zero_grad()
-  direct.zero_grad()
   xy = torch.normal(
     mean=torch.zeros(batch_size, 2),
     std=1
   )
   z = (xy[:, 0] * xy[:, 1])
-  predicted = model(xy, iters=train_iters)
-  direct_predicted = direct(xy)
-  loss = loss_fn(predicted, z)
-  direct_loss = loss_fn(direct_predicted, z)
-  loss.backward()
-  direct_loss.backward()
-  for p in model.parameters():
-    p.data.add_(other=p.grad.data, alpha=-lr)
-  for p in direct.parameters():
-    p.data.add_(other=p.grad.data, alpha=-lr)
-  with torch.no_grad():
-    eq_loss = loss_fn(model(xy, iters=max_iters), z)
-  losses[f'iter {train_iters}'].append(loss.item())
-  losses[f'iter {max_iters}'].append(eq_loss.item())
-  losses[f'traditional'].append(direct_loss.item())
-  s = ''
-  for k, v in losses.items():
-    s += f'  {k} {v[-1]})'
+  cos_mult = 1.00001 + np.cos(i * np.pi / train_batches)
+  s = f'{i=}'
+  for k, m in models.items():
+    m.zero_grad()
+    predicted = m(xy, iters=train_iters)
+    loss = loss_fn(predicted, z)
+    loss.backward()
+    for p in m.parameters():
+      p.data.add_(other=p.grad.data, alpha=-lr)
+    losses[k].append(loss.item())
+    s += f'  {k} {loss.item():.6f})'
   print(s)
 
-#print(model.l0.data)
-#print(model.l1.data)
+print('\nEVAL\n=====')
+eval_size = 10000
+xy = torch.normal(
+  mean=torch.zeros(eval_size, 2),
+  std=1
+)
+z = (xy[:, 0] * xy[:, 1])
+with torch.no_grad():
+  for k, m in models.items():
+    predicted = m(xy, iters=train_iters)
+    loss = loss_fn(predicted, z)
+    full_k = '  ' + k + ' ' * (10 - len(k))
+    print(f'  {full_k} {loss.item():.6f})')
+
+#for k, m in models.items():
+#  if 'main' in k:
+#    print(f'\n{k} weights ====')
+#    print(m.wih.data)
+#    print(m.whb.data)
+#    print(m.why.data)
+
 for k, seq in losses.items():
   plt.plot(seq, label=k)
+plt.legend()
+plt.show()
+
+smoothing = 10
+def smooth(seq):
+  return np.mean(np.reshape(seq, [-1, smoothing]), axis=1)
+for k, seq in losses.items():
+  smoothed_x = smooth(np.arange(len(seq)))
+  smoothed_y = smooth(seq)
+  plt.plot(smoothed_x, smoothed_y, label=k)
 plt.legend()
 plt.show()
 
